2e6423c5e790b5cdcdd7dc32c82a6bcafeea020f
Enrique <enrique.santafe@hotmail.com>
2022-06-07 14:29:11 +0200

    pagina registro terminada

--- frontend/src/components/paginas/Registro.jsx
+++ frontend/src/components/paginas/Registro.jsx
@@ -1,7 +1,55 @@
+import { useState } from 'react'
+import axios from 'axios'
+
 const Registro = () => {
+  const [input, setInput] = useState({
+    nombre: '',
+    email: '',
+    contraseña: ''
+  })
+  const handleChange = (e) => { //cambia el valor por el que escribe el usuario
+    /* console.log(e) */
+    /* console.log(input) */
+    const {name, value} = e.target
+    setInput(prevInput => { //guarda el valor previo
+      return {
+        ...prevInput,
+        [name]: value
+      }
+    })
+  }
+  const handleClick = (e) => { //se activa cuando se pulsa el botón
+    /* console.log(e) */
+    e.preventDefault()
+    const nuevoUsuario = { //crea objeto para pasarlo al servidor
+      nombre: input.nombre,
+      email: input.email,
+      contraseña: input.contraseña
+    }
+    axios.post('/registrar', nuevoUsuario) // pasa los datos al servidor o base de datos
+  }
   return (
     <div className='container micontenedor'>
       <h1>Registro</h1>
+      <p>A continuación puedes registrarte</p>
+      <main className="form-signin w-100 m-auto">
+        <form>
+          <div className="form-floating">
+            <input onChange={handleChange} name='nombre' value={input.name} type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Nombre</label>
+          </div>
+          <div className="form-floating">
+            <input onChange={handleChange} name='email' value={input.email} type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+            <label htmlFor="floatingInput">Email address</label>
+          </div>
+          <div className="form-floating mb-3">
+            <input onChange={handleChange} name='contraseña' value={input.contraseña} type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+            <label htmlFor="floatingPassword">Password</label>
+          </div>
+          <button onChange={handleClick} className="w-100 btn btn-lg btn-primary" type="submit">Registrar</button>
+          <p className="mt-5 mb-3 text-muted text-center">&copy; 2017–2022</p>
+        </form>
+      </main>
     </div>
   )
 }
--- frontend/src/components/partials/Footer.jsx
+++ frontend/src/components/partials/Footer.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <footer>
-      <div class="container">
-        <footer class="py-3 my-4">
-          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item">
-              <Link to="/" class="nav-link px-2 text-muted">Home</Link>
+      <div className="container">
+        <footer className="py-3 my-4">
+          <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+            <li className="nav-item">
+              <Link to="/" className="nav-link px-2 text-muted">Home</Link>
             </li>
-            <li class="nav-item">
-              <Link to="/registro" class="nav-link px-2 text-muted">Registro</Link>
+            <li className="nav-item">
+              <Link to="/registro" className="nav-link px-2 text-muted">Registro</Link>
             </li>
           </ul>
-          <p class="text-center text-muted">&copy; 2022 Company, Inc</p>
+          <p className="text-center text-muted">&copy; 2022 Company, Inc</p>
         </footer>
       </div>
     </footer>
